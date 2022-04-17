20eb3868eab43b9d7a2b6a609e48c2a6f00aaa95
Ishmeet Singh Saggu <57167867+Psychik-N@users.noreply.github.com>
2022-04-17 12:49:39 +0530

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,7 +8,25 @@ public class CalculatorTest {
 
     @Test
     public void testingComputeFactorial(){
-        assertEquals("computing Factorial of 3", 6, calculator.computeFactorial(3));
-        assertNotEquals("computing Factorial of 4", 8, calculator.computeFactorial(4));
+        assertEquals("computing Factorial", 6, calculator.computeFactorial(3));
+        assertNotEquals("computing Factorial", 8, calculator.computeFactorial(4));
+    }
+    
+    @Test
+    public void testingComputePower(){
+        assertEquals("computing Power", 8, calculator.computePower(2, 3), DELTA);
+        assertNotEquals("computing Power", 32, calculator.computeFactorial(2, 4), DELTA);
+    }
+    
+    @Test
+    public void testingComputeLog(){
+        assertEquals("computing Natural Log", 0, calculator.computeLog(1), DELTA);
+        assertNotEquals("computing Natural Log", 32, calculator.computeFactorial(2.1), DELTA);
+    }
+    
+    @Test
+    public void testingComputeSQ(){
+        assertEquals("computing SQ", 7, calculator.computeSQ(49), DELTA);
+        assertNotEquals("computing SQ", 4, calculator.computeFactorial(4), DELTA);
     }
 }
